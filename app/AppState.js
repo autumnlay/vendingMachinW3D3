b6a3fc8d0d446ae5e2cb25da463630871c057dfd
Autumn Lay <autumn.lay0@gmail.com>
2021-10-19 15:14:13 -0600

    mid update

--- app/AppState.js
+++ app/AppState.js
@@ -1,9 +1,16 @@
+import { Snack } from "./Models/Vender.js"
 import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
 
 class AppState extends EventEmitter {
   /** @type {import('./Models/Value').Value[]} */
   values = []
+  /** @type {Snack[]} */
+  snacks = [
+    new Snack('Mountain Sprite', 2.50),
+    new Snack('Root Dew', 5.75)
+  ]
+
 }
 
 export const ProxyState = new Proxy(new AppState(), {
--- app/main.js
+++ app/main.js
@@ -1,7 +1,9 @@
 import { ValuesController } from "./Controllers/ValuesController.js";
+import { VenderControl } from "./Controllers/VenderControl.js";
 
 class App {
   valuesController = new ValuesController();
+  venderController = new VenderControl();
 }
 
 window["app"] = new App();
